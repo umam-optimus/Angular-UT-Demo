7c5a76ee776a45db90eeb033878d86dd6f34162b
umam-optimus <uma.maheshwari@optimusinfo.com>
2022-06-27 16:53:40 +0530

    updated karma file

--- angular-pagination-main/karma.conf.js
+++ angular-pagination-main/karma.conf.js
@@ -27,10 +27,11 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-pagination'),
+      dir: require('path').join(__dirname, './coverage/'),
       // subdir: '.',
       reporters: [
         { type: 'html' },
+        { type: 'lcovonly' },
         { type: 'text-summary' },
         { type: 'cobertura' }
       ],
